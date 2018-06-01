7063e2593a2a2b4668250e2f27ce3f230f02f152
Vinod Kumar Nair <vinod.kumar13@sap.com>
2018-06-01 14:39:03 +0530

    updated index file again for port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express() 
-const port = 5050 
+const port = 5000 
 app.use(bodyParser.json()) 
 
 app.post('/', (req, res) => {
